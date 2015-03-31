d3fc0504d861902f53013e42c4bb001e3e0ef1eb
Ludwin <luwin777@gmail.com>
2015-03-31 16:42:47 -0600

    Funcionalidad replace db by user terminada

--- wp-content/themes/plusvida/page-guardarpeso.php
+++ wp-content/themes/plusvida/page-guardarpeso.php
@@ -9,8 +9,10 @@ get_header(); ?>
 		<div id="content" role="main">
 
 <?php
+
 $usuario = get_current_user_id( );
-$fechaPorGuardar = $_POST['fecha'];
+$fechaPorGuardar = '2015,03,09';//$_POST['fecha'];
+$table_name = "pesos_plusvida";
 $fechaDePeso =  $wpdb->get_col( "SELECT fecha FROM pesos_plusvida WHERE usuario=$usuario" );
 /////////////////////// si el peso ya existe y hay que sobre escribirlo
 if (in_array($_POST['fecha'], $fechaDePeso)){?>
@@ -19,13 +21,12 @@ if (in_array($_POST['fecha'], $fechaDePeso)){?>
 </script>
 <?php
 global $wbpd;
+$wpdb->show_errors();
+//$wpdb->query($wpdb->prepare("DELETE FROM pesos_plusvida WHERE usuario = '%d' AND fecha = '%c'", $usuario, $fechaPorGuardar));
+$wpdb->delete( $table_name, array( 'fecha' => $fechaPorGuardar, 'usuario' => $usuario));
+echo $fechaPorGuardar;
 
-$wpdb->query($wpdb->prepare("DELETE FROM pesos_plusvida WHERE usuario = '%d' AND fecha = '%c'", $usuario, $fechaPorGuardar));
 
-
-
-
-$table_name = "pesos_plusvida";
 $wpdb->insert( $table_name, array( 'usuario' => $_POST['usuario'], 'peso' => $_POST['peso'],
 																		'fecha' => $_POST['fecha'], 'tipodia' => $_POST['tipodia']) );?>
 
