74009a756912b49eb1c9f5fb1e936fa8babd3889
Ludwin <luwin777@gmail.com>
2015-09-23 08:23:27 -0600

    Acortion menu for admin

--- wp-admin/index.php
+++ wp-admin/index.php
@@ -93,20 +93,46 @@ include( ABSPATH . 'wp-admin/admin-header.php' );
 ?>
 
 <div class="wrap">
-	<h2><?php echo esc_html( $title ); ?></h2>
+	<h2>Bienvenido(a), <?php global $current_user; if ( isset($current_user) ) { echo $current_user->display_name;}?></h2>
 
-<style>
-.tutoriales{ background-color: #fff; margin: 18px 0; padding: 20px; width: 95%;}
+<head>
+ <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
 
-</style>
 
-<div class="tutoriales"><center><h3>¿Cómo utilizar el sitio web?</h3></center>
-	<ul>
-		<li><img src="images/">¿Cómo crear usuarios?</li>
-		<li><img src="images/">¿Cómo cargar audios?</li>
-		<li><img src="images/">¿Cómo crear recomendaciones Dr. PlusVida?</li>
-		<li><img src="images/">¿Cómo crear páginas estáticas?</li>
-	</ul>
+<div class="tutoriales"><center><h3>¡Administrar este sitio web es fácil!</h3></br>Sigue las instrucciones que encontrarás a continuaciónz:</center>
+
+	<section class="ac-container">
+		<div>
+			<input id="ac-1" name="accordion-1" type="radio" checked />
+			<label for="ac-1">¿Cómo crear usuarios?</label>
+			<article class="ac-small">
+				<p>Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows.</p>
+			</article>
+		</div>
+		<div>
+			<input id="ac-2" name="accordion-1" type="radio" />
+			<label for="ac-2">¿Cómo cargar audios?</label>
+			<article class="ac-medium">
+				<p>Like you, I used to think the world was this great place where everybody lived by the same standards I did, then some kid with a nail showed me I was living in his world, a world where chaos rules not order, a world where righteousness is not rewarded. That's Cesar's world, and if you're not willing to play by his rules, then you're gonna have to pay the price. </p>
+			</article>
+		</div>
+		<div>
+			<input id="ac-3" name="accordion-1" type="radio" />
+			<label for="ac-3">¿Cómo crear recomendaciones Dr. PlusVida?</label>
+			<article class="ac-large">
+				<p>You think water moves fast? You should see ice. It moves like it has a mind. Like it knows it killed the world once and got a taste for murder. After the avalanche, it took us a week to climb out. Now, I don't know exactly when we turned on each other, but I know that seven of us survived the slide... and only five made it out. Now we took an oath, that I'm breaking now. We said we'd say it was the snow that killed the other two, but it wasn't. Nature is lethal but it doesn't hold a candle to man. </p>
+			</article>
+		</div>
+		<div>
+			<input id="ac-4" name="accordion-1" type="radio" />
+			<label for="ac-4">¿Cómo crear páginas estáticas?</label>
+			<article class="ac-large">
+				<p>You see? It's curious. Ted did figure it out - time travel. And when we get back, we gonna tell everyone. How it's possible, how it's done, what the dangers are. But then why fifty years in the future when the spacecraft encounters a black hole does the computer call it an 'unknown entry event'? Why don't they know? If they don't know, that means we never told anyone. And if we never told anyone it means we never made it back. Hence we die down here. Just as a matter of deductive logic. </p>
+			</article>
+		</div>
+	</section>
+
 </div>
 
 
