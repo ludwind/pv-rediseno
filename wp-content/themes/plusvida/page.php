6a9a54b71fb7d155e6d91d6e0e8b71897955ae4d
Ludwin <luwin777@gmail.com>
2016-05-13 23:05:03 -0600

    Fix soundcloud en movil

--- wp-content/themes/plusvida/page.php
+++ wp-content/themes/plusvida/page.php
@@ -57,13 +57,16 @@ if (count($usuarioFechaInicial) == 0) { ?>
 <section>
 	<center>
 		<div class="soundcloud3"></div><div class="soundcloud"></div><div class="soundcloud2"></div>
-		<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/17787358%3Fsecret_token%3Ds-YizhM&amp;color=0066cc&amp;auto_play=false&amp;hide_related=false&amp;show_comments=false&amp;show_user=true&amp;show_reposts=false"></iframe>
+		<iframe width="100%" height="450" scrolling="no" frameborder="no"
+src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/17787358%3Fsecret_token%3Ds-YizhM&amp;color=0066cc&amp;auto_play=false&amp;hide_related=false&amp;show_comments=false&amp;show_user=true&amp;show_reposts=false"></iframe>
 		</br>
 		<div class="soundcloud3"></div><div class="soundcloud"></div><div class="soundcloud2"></div>
-		<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/43738728%3Fsecret_token%3Ds-ApxNf&amp;color=0066cc&amp;auto_play=false&amp;hide_related=false&amp;show_comments=false&amp;show_user=true&amp;show_reposts=false"></iframe>
+		<iframe width="100%" height="450" scrolling="no" frameborder="no"
+src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/43738728%3Fsecret_token%3Ds-ApxNf&amp;color=0066cc&amp;auto_play=false&amp;hide_related=false&amp;show_comments=false&amp;show_user=true&amp;show_reposts=false"></iframe>
 		</br>
 		<div class="soundcloud3"></div><div class="soundcloud"></div><div class="soundcloud2"></div>
-		<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/107872399%3Fsecret_token%3Ds-1dhzt&amp;color=0066cc&amp;auto_play=false&amp;hide_related=false&amp;show_comments=false&amp;show_user=true&amp;show_reposts=false"></iframe>
+		<iframe width="100%" height="450" scrolling="no" frameborder="no"
+src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/107872399%3Fsecret_token%3Ds-1dhzt&amp;color=0066cc&amp;auto_play=false&amp;hide_related=false&amp;show_comments=false&amp;show_user=true&amp;show_reposts=false"></iframe>
 	</center>
 	
 </section>
