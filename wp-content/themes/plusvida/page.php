cc01fed0f6f6f39b53424b5fbb97c95607b38b6b
Ludwin <luwin777@gmail.com>
2016-05-19 23:49:36 -0600

    bloque de audios 4

--- wp-content/themes/plusvida/page.php
+++ wp-content/themes/plusvida/page.php
@@ -67,6 +67,10 @@ src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists
 		<div class="soundcloud3"></div><div class="soundcloud"></div><div class="soundcloud2"></div>
 		<iframe width="100%" height="450" scrolling="no" frameborder="no"
 src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/107872399%3Fsecret_token%3Ds-1dhzt&amp;color=0066cc&amp;auto_play=false&amp;hide_related=false&amp;show_comments=false&amp;show_user=true&amp;show_reposts=false"></iframe>
+		</br>
+		<div class="soundcloud3"></div><div class="soundcloud"></div><div class="soundcloud2"></div>
+		<iframe width="100%" height="450" scrolling="no" frameborder="no"
+src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/225775986%3Fsecret_token%3Ds-OCLVN&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>
 	</center>
 	
 </section>
